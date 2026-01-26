668745cba2586e3a4223015358802585f8c3a3de
redli <dorahero2727@gmail.com>
2026-01-27 00:52:03 +0800

    refactor: 移動 `.env` 檔案至 `src/data/` 並更新 Vite 配置。

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   base: './', // 設定為相對路徑，這樣部署到任何子目錄都能運作
+  envDir: 'src/data/env',
   plugins: [vue()],
 })
